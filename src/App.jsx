1f48950aa1159cdc0e525532a3b6e262e0c7e656
Peter Osoo <posoo617@gmail.com>
2024-02-28 17:01:18 +0300

    display users

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 const App = () => {
-	return <div>Github Users</div>
+	const users = ["Jake", "Mike", "Biggie", "Layla"]
+	console.log("App running")
+	return (
+		<div>
+			<h3> Github Users </h3>
+			<p> {users[0]} </p>
+			<p> {users[1]} </p>
+			<p> {users[2]} </p>
+			<p> {users[3]} </p>
+		</div>
+	)
 }
 
 export default App
